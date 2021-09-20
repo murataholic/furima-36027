169c29d8b059f89466d59ea6b47165c58f61b8da
murataholic <murataholic@gmail.com>
2021-09-21 01:38:47 +0900

    バリデーションの追加（金額入力の下限・上限設定）

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,10 +2,13 @@ class Item < ApplicationRecord
   belongs_to :user
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
+  belongs_to :category, :condition, :delivery_fee, :prefecture, :shipping_day
 
   with_options presence: true do
-    validates :name, :description, :price
+    validates :name, :description
+    with_options numericality: { in: 300..9999999 } do 
+      validates :price
+    end
     with_options numericality: { other_than: 1, message: "can't be blank" } do
       validates :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :shipping_day_id
     end
