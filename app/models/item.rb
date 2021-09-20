9ee513ebfac966cb0ce5cab6763221f068cb3314
murataholic <murataholic@gmail.com>
2021-09-21 01:03:42 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,16 @@
 class Item < ApplicationRecord
   belongs_to :user
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+
+  with_options presence: true do
+    validates :name, :description, :price
+    with_options numericality: { other_than: 1, message: "can't be blank" } do
+      validates :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :shipping_day_id
+    end
+    with_options foreign_key: true do
+      validates :user
+    end
+  end
 end
