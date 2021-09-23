0189505406fa36ad3581d48c747e71d19e123bdf
murataholic <murataholic@gmail.com>
2021-09-23 14:38:41 +0900

    createアクションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,20 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to action: :index
+    else
+      render :new
+    end
   end
 
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :name, :description, :category_id, :condition_id,
+      :delivery_fee_id, :prefecture_id, :shipping_day_id, :price, :image
+    ).merge(user_id: current_user.id)
+  end
 end
