34c6a8917242708973a89a5b4f4c32fe09eb67a9
murataholic <murataholic@gmail.com>
2021-09-23 15:32:57 +0900

    ビューファイル等の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,8 +13,5 @@ class Item < ApplicationRecord
     with_options numericality: { other_than: 1, message: "can't be blank" } do
       validates :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :shipping_day_id
     end
-    with_options foreign_key: true do
-      validates :user
-    end
   end
 end
