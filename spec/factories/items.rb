98bf01fd5a0f59358ccb5dd766e69980a5bf491d
murataholic <murataholic@gmail.com>
2021-09-24 15:03:34 +0900

    FactoryBotの設定

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+    image           { Faker::Lorem.sentence }
+    name            { Faker::Name.initials(number: 4) }
+    description     { Faker::Lorem.sentence }
+    category_id     { rand(2..11) }
+    condition_id    { rand(2..7) }
+    delivery_fee_id { rand(2..3) }
+    prefecture_id   { rand(2..48) }
+    shipping_day_id { rand(2..4) }
+    price           { Faker::Number.between(from: 300, to: 9999999) }
+    association :user
   end
 end
