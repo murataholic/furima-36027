9862369ae06b6b4a3b4ba92c9c4bfd8a4ef7eaa0
murataholic <murataholic@gmail.com>
2021-10-07 01:16:44 +0900

    保存したいカラム名の設定

--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -1,3 +1,4 @@
 class PurchaseShippingAddress
   include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city_ward_town_village, :house_number, :building_name, :telephone_number, :user_id, :item_id, :purchase_id 
 end
