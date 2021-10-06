92b8cd80db23a83b4bc249fc9183e369e7c8063b
murataholic <murataholic@gmail.com>
2021-10-07 01:25:51 +0900

    バリデーションの修正

--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -1,4 +1,12 @@
 class PurchaseShippingAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city_ward_town_village, :house_number, :building_name, :telephone_number, :user_id, :item_id, :purchase_id 
+  attr_accessor :postal_code, :prefecture_id, :city_ward_town_village, :house_number, :building_name, :telephone_number, :user_id, :item_id, :purchase_id
+
+  with_options presence: true do
+    validates :user_id
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :telephone_number, format: { with: /\A\d{10}$|^\d{11}\z/, message: "is invalid. Don't include hyphen(-) nor do use full-width characters" }
+  end
+  validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
+  
 end
