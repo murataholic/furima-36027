f65ee42538aa10f7a99bb5cb837e6be0d821b468
murataholic <murataholic@gmail.com>
2021-10-07 01:32:36 +0900

    データを保存する処理

--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -9,4 +9,8 @@ class PurchaseShippingAddress
   end
   validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
   
+  def save
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city_ward_town_village: city_ward_town_village, house_number: house_number, building_name: building_name, telephone_number: telephone_number, purchase_id: purchase.id)
+  end
 end
