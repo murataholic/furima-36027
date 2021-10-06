76b5866cc165a44600d62d1980f65abb88e4b08f
murataholic <murataholic@gmail.com>
2021-10-07 01:04:20 +0900

    バリデーションの設定

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,3 +1,7 @@
 class ShippingAddress < ApplicationRecord
   belongs_to :purchase
+
+  validates :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
+  validates :telephone_number, presence: true, format: { with: /\A\d{10}$|^\d{11}\z/, message: "is invalid. Don't include hyphen(-) nor do use full-width characters" }
 end
