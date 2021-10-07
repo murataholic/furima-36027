9ae8cbe46108d5ba71568f4b42a9b783cfcd7f09
murataholic <murataholic@gmail.com>
2021-10-07 13:44:56 +0900

    アソシエーションの編集２

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -2,8 +2,4 @@ class Purchase < ApplicationRecord
   belongs_to :user
   belongs_to :item
   has_one :shipping_address
-  
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :prefecture
-  
 end
